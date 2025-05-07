a3e31ce5a0e65260b8cdf55451c13d44a284c7a4
arti <artichaudhary0>
2025-05-07 16:07:30 +0530

    second commit

---
+++ DATATYPE.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+    int a = 20;
+    float b = 59.20;
+    char c = 'a';
+    clrscr();
+    
+    printf("value of a is : %d",a);
+    printf("value of b is : %f",b);
+    printf("value of c is : %c",c);
+
+    getch();
+}
